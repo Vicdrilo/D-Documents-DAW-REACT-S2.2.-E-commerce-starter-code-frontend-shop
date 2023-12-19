542f5ca438d071f038a486a47ba2b636dc1aaa78
Victor Aguado Navarro <aguadonavarrovictor@gmail.com>
2023-12-19 09:39:51 +0100

    Ejercicio 6 (validación) finalizado

--- js/checkout.js
+++ js/checkout.js
@@ -2,6 +2,8 @@
 // Exercise 6
 function validate() {
 	var error = 0;
+	let errorMessage = "";
+
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
@@ -20,50 +22,80 @@ function validate() {
 
 	//Expresiones Regulares
 	let nameRegex = /[A-Za-z]{3,}/g;
+	let lastNameRegex = /[A-Za-z]{3,}/g;
 	let mailRegex = /^\w+([\.-]?\w*)*@\w+\.(es|com)$/g;
 	let phoneRegex = /^6{1}(\d){8}/g;
 	let pswdRegex = /[A-Za-z0-9]{4,8}/;
 	
+	
 	// Validate fields entered by the user: name, phone, password, and email
 	if(fName.value == "" || !nameRegex.test(fName.value)){
+		console.log('Valor fLastN: '+fName.value);
+		console.log('Validación RegEx fLastN: '+nameRegex.test(fName.value));
 		error++;
+		errorMessage += 'Nombre error. ';
 		fName.classList.add('is-invalid');
 		errorName.style.display = 'block';
+	}else{
+		fName.classList.remove('is-invalid');
+		errorName.style.display = 'none';
 	}	
 
 	if(fEmail.value == "" || fEmail.value.length < 3 || !mailRegex.test(fEmail.value)){
 		error++;
-		fName.classList.add('is-invalid');
+		errorMessage += 'Email error. ';
+		fEmail.classList.add('is-invalid');
 		errorEmail.style.display = 'block';
+	}else{
+		fEmail.classList.remove('is-invalid');
+		errorEmail.style.display = 'none';
 	}
 
 	if(fAddress.value == "" || fAddress.value.length < 3){
 		error++;
+		errorMessage += 'Direccion error. ';
 		fAddress.classList.add('is-invalid');
 		errorAddress.style.display = 'block';
+	}else{
+		fAddress.classList.remove('is-invalid');
+		errorAddress.style.display = 'none';
 	}
 
-	if(fLastN.value == "" || !nameRegex.test(fLastN.value)){
+	if(fLastN.value == "" || !lastNameRegex.test(fLastN.value)){
+		console.log('Valor fLastN: '+fLastN.value);
+		console.log('Validación RegEx fLastN: '+lastNameRegex.test(fLastN.value));
 		error++;
+		errorMessage += 'Apellido error. ';
 		fLastN.classList.add('is-invalid');
 		errorLastN.style.display = 'block';
+	}else{
+		fLastN.classList.remove('is-invalid');
+		errorLastN.style.display = 'none';
 	}
 
 	if(fPassword.value == "" || !pswdRegex.test(fPassword.value)){
 		error++;
+		errorMessage += 'Password error. ';
 		fPassword.classList.add('is-invalid');
 		errorPassword.style.display = 'block';
+	}else{
+		fPassword.classList.remove('is-invalid');
+		errorPassword.style.display = 'none';
 	}
 
 	if(fPhone.value == "" || !phoneRegex.test(fPhone.value)){
 		error++;
+		errorMessage += 'Movil error. ';
 		fPhone.classList.add('is-invalid');
 		errorPhone.style.display = 'block';
+	}else{
+		fPhone.classList.remove('is-invalid');
+		errorPhone.style.display = 'none';
 	}
 
 	 
 	if(error>0){
-		alert("Error");
+		alert("Error" + errorMessage);
 	}else{
 		alert("OK");
 	}
--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,7 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
+//import products from './products.json' assert{type: 'json'};
+
+
 var products = [
     {
         id: 1,
